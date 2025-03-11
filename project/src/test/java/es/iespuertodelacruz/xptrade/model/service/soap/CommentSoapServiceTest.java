54547257328bb44d4db56b26d73b753e49218b29
nalleon <nabil14716@gmail.com>
2025-03-11 12:46:03 +0000

    test(service): Added soap services test

---
+++ project/src/test/java/es/iespuertodelacruz/xptrade/model/service/soap/CommentSoapServiceTest.java
@@ -0,0 +1,220 @@
+package es.iespuertodelacruz.xptrade.model.service.soap;
+
+import es.iespuertodelacruz.xptrade.domain.*;
+import es.iespuertodelacruz.xptrade.model.entities.CommentEntity;
+import es.iespuertodelacruz.xptrade.model.repository.ICommentEntityRepository;
+import es.iespuertodelacruz.xptrade.model.service.rest.CommentEntityService;
+import es.iespuertodelacruz.xptrade.utilities.TestUtilities;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.*;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+public class CommentSoapServiceTest extends TestUtilities {
+    @Mock
+    ICommentEntityRepository repositoryMock;
+
+    @Mock
+    CommentEntityService serviceMock;
+
+    @InjectMocks
+    CommentSoapService service;
+
+    User user;
+
+    Post post;
+
+    Comment item;
+
+    Game game;
+    Genre genre;
+    Region region;
+    Publisher publisher;
+    Developer developer;
+    Platform platform;
+    Set<Developer> developerSet;
+    Set<Genre> genreSet;
+    Set<Platform> platformSet;
+    Set<Publisher> publisherSet;
+    Set<Region> regionSet;
+
+    @BeforeEach
+    public void beforeEach() {
+        genre = new Genre(NAME);
+        region = new Region(NAME);
+        publisher = new Publisher(NAME);
+        developer = new Developer(NAME);
+        platform = new Platform(NAME);
+
+        genre.setId(ID);
+        region.setId(ID);
+        publisher.setId(ID);
+        developer.setId(ID);
+        platform.setId(ID);
+
+        developerSet = new HashSet<>(Collections.singletonList(developer));
+        genreSet = new HashSet<>(Collections.singletonList(genre));
+        platformSet = new HashSet<>(Collections.singletonList(platform));
+        publisherSet = new HashSet<>(Collections.singletonList(publisher));
+        regionSet = new HashSet<>(Collections.singletonList(region));
+
+        game = new Game(TITLE, COVER_ART, developerSet, genreSet, platformSet, publisherSet, regionSet);
+        game.setId(ID);
+        user = new User(ID);
+        user.setUsername(NAME);
+
+        post = new Post();
+        post.setId(ID);
+        post.setUser(user);
+        post.setGame(game);
+        
+        item = new Comment(ID);
+        item.setUser(user);
+        item.setContent(CONTENT);
+        item.setCreationDate(CREATION_DATE);
+        item.setPost(post);
+
+        MockitoAnnotations.openMocks(this);
+        serviceMock = new CommentEntityService();
+        serviceMock.setRepository(repositoryMock);
+
+        service = new CommentSoapService();
+        service.setService(serviceMock);
+    }
+    @Test
+    void getAllTest() {
+        List<CommentEntity> list = new ArrayList<>();
+        list.add(new CommentEntity());
+        when(repositoryMock.findAll()).thenReturn(list);
+        Assertions.assertNotNull(service.findAll(), MESSAGE_ERROR);
+    }
+
+    @Test
+    void getByIdNullTest() {
+        Assertions.assertNull(service.findById(1), MESSAGE_ERROR);
+    }
+
+    @Test
+    void getByIdNameNullTest() {
+        Assertions.assertNull(service.findAllByUser(null), MESSAGE_ERROR);
+    }
+
+    @Test
+    void getByIdGameNullTest() {
+        Assertions.assertNull(service.findAllBySubject(null), MESSAGE_ERROR);
+    }
+
+
+    @Test
+    void getOneTest() {
+        when(repositoryMock.findById(1)).thenReturn(Optional.of(new CommentEntity()));
+        Assertions.assertNotNull(service.findById(1), MESSAGE_ERROR);
+    }
+
+
+    @Test
+    void getByNameTest() {
+        List<CommentEntity> list = new ArrayList<>();
+        list.add(new CommentEntity());
+        when(repositoryMock.findAllByUser(user.getId())).thenReturn(list);
+        Assertions.assertNotNull(service.findAllByUser(user), MESSAGE_ERROR);
+    }
+
+    @Test
+    void getByGameTest() {
+        List<CommentEntity> list = new ArrayList<>();
+        list.add(new CommentEntity());
+        when(repositoryMock.findAllByUser(user.getId())).thenReturn(list);
+        Assertions.assertNotNull(service.findAllByUser(user), MESSAGE_ERROR);
+    }
+
+
+
+    @Test
+    void addTest() {
+        when(repositoryMock.existsById(1)).thenReturn(false);
+
+        when(repositoryMock.save(any(CommentEntity.class))).thenReturn(new CommentEntity());
+
+        Assertions.assertNotNull(service.save(item), MESSAGE_ERROR);
+    }
+
+
+    @Test
+    void addNullTest() {
+        Assertions.assertNull(service.save(null), MESSAGE_ERROR);
+    }
+
+//    @Test
+//    void updateExceptionTest() throws Exception {
+//        when(repositoryMock.findUserByName(NAME)).thenThrow(new RuntimeException());
+//        Assertions.assertThrows(RuntimeException.class, () -> service.update(new User(1)), MESSAGE_ERROR);
+//    }
+
+
+    @Test
+    void addExceptionTest() throws Exception {
+        when(repositoryMock.save(any(CommentEntity.class))).thenThrow(new RuntimeException());
+        Assertions.assertThrows(RuntimeException.class, () -> service.save(new Comment(1)), MESSAGE_ERROR);
+    }
+
+    @Test
+    void updateTest() throws Exception {
+        when(repositoryMock.findById(post.getId())).thenReturn(Optional.of(new CommentEntity()));
+        Assertions.assertNotNull(service.update(item), MESSAGE_ERROR);
+    }
+
+    @Test
+    void updateFalseTest() throws Exception {
+        Assertions.assertNull(service.update(null), MESSAGE_ERROR);
+    }
+
+    @Test
+    void updateNotFoundTest() throws Exception {
+        when(repositoryMock.findById(post.getId())).thenReturn(Optional.empty());
+        Assertions.assertNull(service.update(item), MESSAGE_ERROR);
+    }
+
+//    @Test
+//    void updateExceptionTest() throws Exception {
+//        Comment item = new Comment();
+//        item.setId(1);
+//        item.setName(NAME);
+//        when(repositoryMock.findByName(item.getName())).thenReturn(Optional.empty());
+//        Assertions.assertNull(service.update(item), MESSAGE_ERROR);
+//    }
+
+    @Test
+    void deleteAdminTest() {
+        when(repositoryMock.findById(1)).thenReturn(Optional.of(new CommentEntity(1)));
+        Assertions.assertFalse(service.delete(1), MESSAGE_ERROR);
+    }
+
+    @Test
+    void deleteNotFoundTest() {
+        when(repositoryMock.findById(1)).thenReturn(Optional.empty());
+        Assertions.assertFalse(service.delete(1), MESSAGE_ERROR);
+    }
+
+    @Test
+    void deleteTest() {
+        when(repositoryMock.deleteEntityById(2)).thenReturn(2);
+        Assertions.assertTrue(service.delete(2), MESSAGE_ERROR);
+    }
+
+
+    @Test
+    void deleteNonExistentTest() {
+        when(repositoryMock.deleteEntityById(1)).thenReturn(0);
+        Assertions.assertFalse(service.delete(1), MESSAGE_ERROR);
+    }
+
+}
+
---
+++ project/src/test/java/es/iespuertodelacruz/xptrade/model/service/soap/RoleSoapServiceTest.java
@@ -0,0 +1,170 @@
+package es.iespuertodelacruz.xptrade.model.service.soap;
+
+import es.iespuertodelacruz.xptrade.domain.Role;
+import es.iespuertodelacruz.xptrade.model.entities.RoleEntity;
+import es.iespuertodelacruz.xptrade.model.repository.IRoleEntityRepository;
+import es.iespuertodelacruz.xptrade.model.service.rest.RoleEntityService;
+import es.iespuertodelacruz.xptrade.utilities.TestUtilities;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+public class RoleSoapServiceTest extends TestUtilities {
+    @Mock
+    IRoleEntityRepository repositoryMock;
+
+    @Mock
+    RoleEntityService serviceMock;
+
+    @InjectMocks
+    RoleSoapService service;
+
+    @BeforeEach
+    public void beforeEach (){
+        MockitoAnnotations.openMocks(this);
+        service = new RoleSoapService();
+        serviceMock = new RoleEntityService();
+        serviceMock.setRepository(repositoryMock);
+        service.setService(serviceMock);
+    }
+    @Test
+    void getAllTest() {
+        List<RoleEntity> list = new ArrayList<>();
+        list.add(new RoleEntity());
+        when(repositoryMock.findAll()).thenReturn(list);
+        Assertions.assertNotNull(service.findAll(), MESSAGE_ERROR);
+    }
+
+    @Test
+    void getByIdNullTest() {
+        Assertions.assertNull(service.findById(1), MESSAGE_ERROR);
+    }
+
+    @Test
+    void getByIdNameNullTest() {
+        Assertions.assertNull(service.findByName(NAME), MESSAGE_ERROR);
+    }
+
+
+    @Test
+    void getOneTest() {
+        when(repositoryMock.findById(1)).thenReturn(Optional.of(new RoleEntity()));
+        Assertions.assertNotNull(service.findById(1), MESSAGE_ERROR);
+    }
+
+
+    @Test
+    void getByNameTest() {
+        when(repositoryMock.findRoleByName(NAME)).thenReturn(Optional.of(new RoleEntity()));
+        Assertions.assertNotNull(service.findByName(NAME), MESSAGE_ERROR);
+    }
+
+
+    @Test
+    void addTest() {
+        when(repositoryMock.existsById(1)).thenReturn(false);
+        Role role = new Role();
+        role.setId(1);
+        role.setName(NAME);
+
+        when(repositoryMock.save(any(RoleEntity.class))).thenReturn(new RoleEntity());
+
+        Assertions.assertNotNull(service.save(role), MESSAGE_ERROR);
+    }
+
+    @Test
+    void addDupeTest() {
+        when(repositoryMock.findRoleByName(NAME)).thenReturn(Optional.of(new RoleEntity()));
+        Role role = new Role();
+        role.setId(1);
+        role.setName(NAME);
+        Assertions.assertNull(service.save(role), MESSAGE_ERROR);
+    }
+
+    @Test
+    void addNullTest() {
+        Assertions.assertNull(service.save(null), MESSAGE_ERROR);
+    }
+
+//    @Test
+//    void updateExceptionTest() throws Exception {
+//        when(repositoryMock.findUserByName(NAME)).thenThrow(new RuntimeException());
+//        Assertions.assertThrows(RuntimeException.class, () -> service.update(new User(1)), MESSAGE_ERROR);
+//    }
+
+
+    @Test
+    void addExceptionTest() throws Exception {
+        when(repositoryMock.save(any(RoleEntity.class))).thenThrow(new RuntimeException());
+        Assertions.assertThrows(RuntimeException.class, () -> service.save(new Role(1)), MESSAGE_ERROR);
+    }
+
+    @Test
+    void updateTest() throws Exception {
+        Role role = new Role();
+        role.setId(1);
+        role.setName(NAME);
+        when(repositoryMock.findRoleByName(role.getName())).thenReturn(Optional.of(new RoleEntity()));
+        Assertions.assertNotNull(service.update(role), MESSAGE_ERROR);
+    }
+
+    @Test
+    void updateFalseTest() throws Exception {
+        Assertions.assertNull(service.update(null), MESSAGE_ERROR);
+    }
+
+    @Test
+    void updateNotFoundTest() throws Exception {
+        Role role = new Role();
+        role.setId(1);
+        role.setName(NAME);
+        when(repositoryMock.findRoleByName(role.getName())).thenReturn(Optional.empty());
+        Assertions.assertNull(service.update(role), MESSAGE_ERROR);
+    }
+
+//    @Test
+//    void updateExceptionTest() throws Exception {
+//        Role role = new Role();
+//        role.setId(1);
+//        role.setName(NAME);
+//        when(repositoryMock.findRoleByName(role.getName())).thenReturn(Optional.empty());
+//        Assertions.assertNull(service.update(role), MESSAGE_ERROR);
+//    }
+
+    @Test
+    void deleteAdminTest() {
+        when(repositoryMock.findById(1)).thenReturn(Optional.of(new RoleEntity("ROLE_ADMIN")));
+        Assertions.assertFalse(service.delete(1), MESSAGE_ERROR);
+    }
+
+    @Test
+    void deleteNotFoundTest() {
+        when(repositoryMock.findById(1)).thenReturn(Optional.empty());
+        Assertions.assertFalse(service.delete(1), MESSAGE_ERROR);
+    }
+
+    @Test
+    void deleteTest() {
+        when(repositoryMock.deleteEntityById(2)).thenReturn(2);
+        Assertions.assertTrue(service.delete(2), MESSAGE_ERROR);
+    }
+
+
+    @Test
+    void deleteNonExistentTest() {
+        when(repositoryMock.deleteEntityById(1)).thenReturn(0);
+        Assertions.assertFalse(service.delete(1), MESSAGE_ERROR);
+    }
+
+}
+
