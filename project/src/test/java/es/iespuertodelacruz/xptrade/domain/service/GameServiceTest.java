f264706b93a773a5a2b10f325166e3a889629eca
jboada <jboada@avantic.net>
2025-03-12 13:07:03 +0000

    test(GameService): finished testing Game Service

--- project/src/test/java/es/iespuertodelacruz/xptrade/domain/service/GameServiceTest.java
+++ project/src/test/java/es/iespuertodelacruz/xptrade/domain/service/GameServiceTest.java
@@ -1,6 +1,6 @@
 package es.iespuertodelacruz.xptrade.domain.service;
 
-import es.iespuertodelacruz.xptrade.domain.Game;
+import es.iespuertodelacruz.xptrade.domain.*;
 import es.iespuertodelacruz.xptrade.domain.interfaces.repository.IGameRepository;
 import es.iespuertodelacruz.xptrade.utilities.TestUtilities;
 import org.junit.jupiter.api.Assertions;
@@ -12,7 +12,9 @@ import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class GameServiceTest extends TestUtilities {
     @Mock
@@ -34,6 +36,55 @@ public class GameServiceTest extends TestUtilities {
         Assertions.assertNull(service.findById(1), MESSAGE_ERROR);
     }
 
+    @Test
+    void findAllByGenreTest() {
+        List<Game> list = new ArrayList<>();
+        list.add(new Game());
+        list.add(new Game());
+        list.add(new Game());
+        Mockito.when(repositoryMock.findAllByGenre(new Genre())).thenReturn(list);
+        Assertions.assertNotNull(service.findAllByGenre(new Genre()), MESSAGE_ERROR);
+    }
+
+    @Test
+    void findAllByDeveloperTest() {
+        List<Game> list = new ArrayList<>();
+        list.add(new Game());
+        list.add(new Game());
+        list.add(new Game());
+        Mockito.when(repositoryMock.findAllByDeveloper(new Developer())).thenReturn(list);
+        Assertions.assertNotNull(service.findAllByDeveloper(new Developer()), MESSAGE_ERROR);
+    }
+
+    @Test
+    void findAllByPlatformTest() {
+        List<Game> list = new ArrayList<>();
+        list.add(new Game());
+        list.add(new Game());
+        list.add(new Game());
+        Mockito.when(repositoryMock.findAllByPlatform(new Platform())).thenReturn(list);
+        Assertions.assertNotNull(service.findAllByPlatform(new Platform()), MESSAGE_ERROR);
+    }
+
+    @Test
+    void findAllByPublisherTest() {
+        List<Game> list = new ArrayList<>();
+        list.add(new Game());
+        list.add(new Game());
+        list.add(new Game());
+        Mockito.when(repositoryMock.findAllByPublisher(new Publisher())).thenReturn(list);
+        Assertions.assertNotNull(service.findAllByPublisher(new Publisher()), MESSAGE_ERROR);
+    }
+
+    @Test
+    void findAllByRegionTest() {
+        List<Game> list = new ArrayList<>();
+        list.add(new Game());
+        list.add(new Game());
+        list.add(new Game());
+        Mockito.when(repositoryMock.findAllByRegion(new Region())).thenReturn(list);
+        Assertions.assertNotNull(service.findAllByRegion(new Region()), MESSAGE_ERROR);
+    }
 
     @Test
     void getOneTest() {
@@ -42,31 +93,51 @@ public class GameServiceTest extends TestUtilities {
     }
 
 
-   /* @Test
+    @Test
     void addTest() {
         Mockito.when(repositoryMock.save(Mockito.any(Game.class))).thenReturn(new Game());
-        Assertions.assertNotNull(service.add("Admin"), MESSAGE_ERROR);
+        Set<Developer> developerSet = new HashSet<>();
+        Set<Genre> genreSet = new HashSet<>();
+        Set<Platform> platformSet = new HashSet<>();
+        Set<Publisher> publisherSet = new HashSet<>();
+        Set<Region> regionSet = new HashSet<>();
+        Assertions.assertNotNull(service.add(TITLE, COVER_ART, developerSet,
+                genreSet, platformSet, publisherSet, regionSet), MESSAGE_ERROR);
     }
 
     @Test
     void addNullTest() {
-        Assertions.assertNull(service.add(null), MESSAGE_ERROR);
+        Assertions.assertNull(service.add(null, null, null, null, null,
+                null, null), MESSAGE_ERROR);
     }
 
     @Test
     void updateExceptionTest() throws Exception {
         Mockito.when(repositoryMock.findById(1)).thenThrow(new RuntimeException("Database error"));
-        Assertions.assertNull(service.update(1, "Admin"), MESSAGE_ERROR);
+        Set<Developer> developerSet = new HashSet<>();
+        Set<Genre> genreSet = new HashSet<>();
+        Set<Platform> platformSet = new HashSet<>();
+        Set<Publisher> publisherSet = new HashSet<>();
+        Set<Region> regionSet = new HashSet<>();
+        Assertions.assertNull(service.update(1, TITLE, COVER_ART, developerSet,
+                genreSet, platformSet, publisherSet, regionSet), MESSAGE_ERROR);
     }
     @Test
     void updateTest() throws Exception {
         Mockito.when(repositoryMock.update(Mockito.any(Game.class))).thenReturn(new Game());
-        Assertions.assertNotNull(service.update(1,"Admin"), MESSAGE_ERROR);
+        Set<Developer> developerSet = new HashSet<>();
+        Set<Genre> genreSet = new HashSet<>();
+        Set<Platform> platformSet = new HashSet<>();
+        Set<Publisher> publisherSet = new HashSet<>();
+        Set<Region> regionSet = new HashSet<>();
+        Assertions.assertNotNull(service.update(1,TITLE, COVER_ART, developerSet,
+                genreSet, platformSet, publisherSet, regionSet), MESSAGE_ERROR);
     }
 
     @Test
     void updateNullTest() throws Exception {
-        Assertions.assertNull(service.update(0, null), MESSAGE_ERROR);
+        Assertions.assertNull(service.update(0, null, null, null, null, null,
+                null, null), MESSAGE_ERROR);
     }
 
     @Test
@@ -86,5 +157,5 @@ public class GameServiceTest extends TestUtilities {
     void deleteFailTest() {
         Mockito.when(repositoryMock.delete(2)).thenReturn(false);
         Assertions.assertFalse(service.delete(2), MESSAGE_ERROR);
-    }*/
+    }
 }
