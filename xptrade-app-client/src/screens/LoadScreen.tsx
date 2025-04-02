bc70dae32f1cce3bbdfc9eadeb513a588430046d
jboada <jboada@avantic.net>
2025-04-02 08:33:48 +0100

    feat(app-client): added basic loading screen

---
+++ xptrade-app-client/src/screens/LoadScreen.tsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+type Props = {}
+
+const LoadScreen = (props: Props) => {
+  return (
+    <View>
+      <Text>Loading...</Text>
+    </View>
+  )
+}
+
+export default LoadScreen
+
+const styles = StyleSheet.create({})
