1ced9a9ed6309383b8147fbd3abe1396fc2b5389
nalleon <nabil14716@gmail.com>
2025-04-16 14:58:24 +0100

    refactor(app-client): Changed utils

--- xptrade-app-client/src/utils/Utils.ts
+++ xptrade-app-client/src/utils/Utils.ts
@@ -1,7 +1,7 @@
 export const URL_API = "http://192.168.1.117:8080/api";
 export const PULLING_INTERVAL = 500;
-// Nabil: 172.16.0.110
+// Nabil: 172.17.0.1
 // Maxi: 192.168.1.117
 
 export const SUCCESS = "success";
-export const RAWG_API = "https://api.rawg.io/api/games?key=1a094ebbcbef4440a46abee88f222abb&search_precise=true&search=";
+export const RAWG_API = "https://api.rawg.io/api/games?key=1a094ebbcbef4440a46abee88f222abb&search=";
