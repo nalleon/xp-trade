043cac67601d61d53027658c5348ef68ce7e5837
mackstm <thelewyntm@gmail.com>
2025-04-16 15:02:02 +0100

    feat(utils): added filter for real this time

--- xptrade-app-client/src/utils/Utils.ts
+++ xptrade-app-client/src/utils/Utils.ts
@@ -4,4 +4,4 @@ export const PULLING_INTERVAL = 500;
 // Maxi: 192.168.1.117
 
 export const SUCCESS = "success";
-export const RAWG_API = "https://api.rawg.io/api/games?key=1a094ebbcbef4440a46abee88f222abb&search=";
+export const RAWG_API = "https://api.rawg.io/api/games?key=1a094ebbcbef4440a46abee88f222abb&search_precise=true&search=";
