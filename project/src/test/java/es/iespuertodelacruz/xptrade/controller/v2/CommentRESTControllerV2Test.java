ca20fb119d8640ee79df3ade67429f4fdd64f5f8
nalleon <nabil14716@gmail.com>
2025-04-24 14:54:07 +0100

    test(controller): #9 Added missing tests for CommentRESTControllerV2

--- project/src/test/java/es/iespuertodelacruz/xptrade/controller/v2/CommentRESTControllerV2Test.java
+++ project/src/test/java/es/iespuertodelacruz/xptrade/controller/v2/CommentRESTControllerV2Test.java
@@ -1,11 +1,13 @@
 package es.iespuertodelacruz.xptrade.controller.v2;
 
 import es.iespuertodelacruz.xptrade.controllers.v2.CommentRESTControllerV2;
-import es.iespuertodelacruz.xptrade.domain.Comment;
+import es.iespuertodelacruz.xptrade.domain.*;
 import es.iespuertodelacruz.xptrade.domain.Post;
-import es.iespuertodelacruz.xptrade.domain.User;
-import es.iespuertodelacruz.xptrade.domain.service.CommentService;
+import es.iespuertodelacruz.xptrade.domain.service.*;
 import es.iespuertodelacruz.xptrade.dto.output.CommentOutputDTO;
+import es.iespuertodelacruz.xptrade.dto.output.PostOutputDTO;
+import es.iespuertodelacruz.xptrade.model.service.rest.CommentEntityService;
+import es.iespuertodelacruz.xptrade.shared.utils.CustomApiResponse;
 import es.iespuertodelacruz.xptrade.utilities.MapperDTOHelper;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -20,11 +22,21 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 
 public class CommentRESTControllerV2Test extends MapperDTOHelper {
     @Mock
     CommentService serviceMock;
+    @Mock
+    UserService serviceUserMock;
+    @Mock
+    PostService servicePostMock;
+    @Mock
+    CommentEntityService entityServiceMock;
+
+    @InjectMocks
+    CommentService serviceMockException;
 
     @InjectMocks
     CommentRESTControllerV2 controller;
@@ -33,8 +45,12 @@ public class CommentRESTControllerV2Test extends MapperDTOHelper {
     @BeforeEach
     public void beforeEach (){
         MockitoAnnotations.openMocks(this);
-        controller = new CommentRESTControllerV2();
         controller.setService(serviceMock);
+        serviceMock.setRepository(entityServiceMock);
+        controller.setService(serviceMock);
+        controller.setPostService(servicePostMock);
+        controller.setUserService(serviceUserMock);
+        serviceMockException.setRepository(entityServiceMock);
     }
     @Test
     void getAllTest() {
@@ -45,8 +61,63 @@ public class CommentRESTControllerV2Test extends MapperDTOHelper {
         when(serviceMock.findAll()).thenReturn(list);
         Assertions.assertNotNull(controller.getAll(), MESSAGE_ERROR);
     }
+    
+    @Test
+    void getAllEmptyTest() {
+        when(serviceMock.findAll()).thenReturn(new ArrayList<>());
+        Assertions.assertEquals(HttpStatus.NO_CONTENT, controller.getAll().getStatusCode(), MESSAGE_ERROR);
+    }
+
+    @Test
+    void getAllByUserTest() {
+        List<Comment> list = new ArrayList<>();
+        list.add(new Comment(1));
+        list.add(new Comment(2));
+        list.add(new Comment(3));
+        when(serviceUserMock.findByUsername(anyString())).thenReturn(new User());
+        when(serviceMock.findByUser(new User())).thenReturn(list);
+        Assertions.assertNotNull(controller.getAllByUser("A"), MESSAGE_ERROR);
+    }
 
 
+    @Test
+    void getAllByUserNoFilterTest() {
+        when(serviceMock.findByUser(new User())).thenReturn(null);
+        Assertions.assertNotNull(controller.getAllByUser("A"), MESSAGE_ERROR);
+    }
+
+    @Test
+    void getAllByUserEmptyTest() {
+        when(serviceUserMock.findByUsername(anyString())).thenReturn(new User());
+        when(serviceMock.findByUser(new User())).thenReturn(new ArrayList<>());
+        Assertions.assertEquals(HttpStatus.NO_CONTENT, controller.getAllByUser("A").getStatusCode(), MESSAGE_ERROR);
+    }
+
+    @Test
+    void getAllByPostTest() {
+        List<Comment> list = new ArrayList<>();
+        list.add(new Comment(1));
+        list.add(new Comment(2));
+        list.add(new Comment(3));
+        when(servicePostMock.findById(any(Integer.class))).thenReturn(new Post());
+        when(serviceMock.findByPost(new Post())).thenReturn(list);
+        Assertions.assertNotNull(controller.getAllByPost(1), MESSAGE_ERROR);
+    }
+
+    @Test
+    void getAllByPostNoFilterTest() {
+        when(serviceMock.findByPost(new Post())).thenReturn(null);
+        Assertions.assertNotNull(controller.getAllByPost(1), MESSAGE_ERROR);
+    }
+
+    @Test
+    void getAllByPostEmptyTest() {
+        when(servicePostMock.findById(any(Integer.class))).thenReturn(new Post());
+        when(serviceMock.findByPost(new Post())).thenReturn(new ArrayList<>());
+        Assertions.assertEquals(HttpStatus.NO_CONTENT, controller.getAllByPost(1).getStatusCode(), MESSAGE_ERROR);
+    }
+
+    
     @Test
     void getOneTest() {
         when(serviceMock.findById(1)).thenReturn(new Comment(1));
@@ -54,14 +125,38 @@ public class CommentRESTControllerV2Test extends MapperDTOHelper {
     }
 
     @Test
+    void getOneNotFoundTest() {
+        when(serviceMock.findById(1)).thenReturn(null);
+        Assertions.assertEquals(HttpStatus.NOT_FOUND, controller.getById(1).getStatusCode(), MESSAGE_ERROR);
+    }
+
+    @Test
     void addTest() {
         when(serviceMock.add(any(Post.class), any(User.class), any(String.class))).thenReturn(new Comment());
         CommentOutputDTO aux = new CommentOutputDTO(1, postOutputDTO, userDTO, CONTENT, CREATION_DATE);
+
 
         ResponseEntity responseEntity = controller.add(aux);
         Assertions.assertEquals(HttpStatus.CREATED, responseEntity.getStatusCode(), MESSAGE_ERROR);
     }
 
+    @Test
+    void addNullTest() {
+        Assertions.assertEquals(HttpStatus.BAD_REQUEST, controller.add(null).getStatusCode(), MESSAGE_ERROR);
+    }
+
+    @Test
+    void addThrowsExceptionTest() {
+        CommentOutputDTO dto = new CommentOutputDTO(1, postOutputDTO, userDTO, CONTENT, CREATION_DATE);
+
+        when(entityServiceMock.save(any(Comment.class))).thenThrow(new RuntimeException());
+
+        controller.setService(serviceMockException);
+
+        ResponseEntity<CustomApiResponse<?>> response = controller.add(dto);
+
+        Assertions.assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, response.getStatusCode(), MESSAGE_ERROR);
+    }
 
     @Test
     void deleteTest() {
