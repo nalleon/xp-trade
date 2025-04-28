37630abe78f4c6def1c5833f43670f016fe1e4f8
jboada <jboada@avantic.net>
2025-04-28 09:29:15 +0100

    feat(feedback): #23 added toString and equals tests

--- project/src/test/java/es/iespuertodelacruz/xptrade/model/documents/FeedbackDocumentTest.java
+++ project/src/test/java/es/iespuertodelacruz/xptrade/model/documents/FeedbackDocumentTest.java
@@ -13,11 +13,36 @@ public class FeedbackDocumentTest extends TestUtilities {
     @BeforeEach
     public void beforeEach() {
         item = new FeedbackDocument();
+        item.setId(TITLE);
         item.setContent(CONTENT);
     }
 
     @Test
     public void getSetTest() {
+        Assertions.assertEquals(TITLE, item.getId(), MESSAGE_ERROR);
         Assertions.assertEquals(CONTENT, item.getContent(), MESSAGE_ERROR);
+    }
+
+    @Test
+    public void toStringTest() {
+        Assertions.assertTrue(item.toString().contains(TITLE), MESSAGE_ERROR);
+        Assertions.assertTrue(item.toString().contains(CONTENT), MESSAGE_ERROR);
+    }
+
+    @Test
+    public void equalsTest(){
+        FeedbackDocument equals = new FeedbackDocument(item.getId(), TITLE);
+
+        FeedbackDocument differentId = new FeedbackDocument();
+        equals.setId("no");
+        String str = "str";
+        FeedbackDocument nullObject = null;
+
+        Assertions.assertEquals(item, equals, MESSAGE_ERROR);
+        Assertions.assertEquals(item, item, MESSAGE_ERROR);
+        Assertions.assertEquals(item.hashCode(), equals.hashCode(), MESSAGE_ERROR);
+        Assertions.assertNotEquals(item, differentId, MESSAGE_ERROR);
+        Assertions.assertNotEquals(item,nullObject,  MESSAGE_ERROR);
+        Assertions.assertNotEquals(item, str, MESSAGE_ERROR);
     }
 }
