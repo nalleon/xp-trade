ac072fc4925db004984debcca1664e8e7d90c8fc
nalleon <nabil14716@gmail.com>
2025-05-10 23:03:57 +0100

    feat(app-client): #28 Modified HomeScreen view

--- xptrade-app-client/src/screens/HomeScreen.tsx
+++ xptrade-app-client/src/screens/HomeScreen.tsx
@@ -52,68 +52,75 @@ function HomeScreen({ navigation }: Props) {
   };
 
 
-  const navigateToPostDetails = (item : PostXPTrade) => {
+  const navigateToPostDetails = (item: PostXPTrade) => {
     context.setCurrentPost(item);
     navigation.navigate('PostScreen')
   }
 
   return (
-    <View className="flex-1 bg-[#0F1218] pt-10">
+    <View className="flex-1 bg-[#0F1218] pt-5">
       <FlatList
         data={posts}
         keyExtractor={(item) => item.id}
-        renderItem={({ item }) => (
-          <View className="bg-[#1E222A] rounded-xl p-4 mb-4 mx-4">
-            <View className="flex-row items-center mb-2">
-              <Image
-                source={
-                  item.user.profilePicture
-                    ? { uri: item.user.profilePicture }
-                    : require('../resources/xp-trade.png')
-                }
-                className="w-10 h-10 rounded-full mr-3"
-              />
-              <View>
-                <TouchableOpacity>
-                  <Text className="text-[#F6F7F7] font-semibold">@{item.user.username}</Text>
-                </TouchableOpacity>
-                <TouchableOpacity>
-                  <Text className="text-xs text-[#8899A6]">{item.game.title}</Text>
-                </TouchableOpacity>
-              </View>
-            </View>
+        renderItem={({ item }) => {
+          const contentLength = item.content.length;
 
-            <View className="h-px bg-[#2C3038] mb-6" />
+          const maxCharactersForFiveLines = 100;
 
-
-            <Text
-              className="text-[#F6F7F7] mb-2"
-              numberOfLines={6}
-            >
-              {item.content}
-
-            </Text>
-
+          return (
             <TouchableOpacity onPress={() => navigateToPostDetails(item)}>
-              <Text className="text-xs text-blue-400">Ver más</Text>
+
+
+              <View className="bg-[#1E222A] rounded-xl p-4 mb-4 mx-4">
+                <View className="flex-row items-center mb-2">
+                  <Image
+                    source={
+                      item.user.profilePicture
+                        ? { uri: item.user.profilePicture }
+                        : require('../resources/xp-trade.png')
+                    }
+                    className="w-10 h-10 rounded-full mr-3"
+                  />
+                  <View>
+                    <TouchableOpacity>
+                      <Text className="text-[#F6F7F7] font-semibold">@{item.user.username}</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity>
+                      <Text className="text-xs text-[#8899A6]">{item.game.title}</Text>
+                    </TouchableOpacity>
+                  </View>
+                </View>
+
+                <View className="h-px bg-[#2C3038] mb-6" />
+
+                <Text
+                  className="text-[#F6F7F7] mb-2"
+                  numberOfLines={contentLength > maxCharactersForFiveLines ? 6 : 0}
+                >
+                  {item.content}
+                </Text>
+
+                {contentLength > maxCharactersForFiveLines && (
+                  <TouchableOpacity onPress={() => navigateToPostDetails(item)}>
+                    <Text className="text-xs text-blue-400">Ver más</Text>
+                  </TouchableOpacity>
+                )}
+
+                {item.picture && (
+                  <Image
+                    source={{ uri: item.picture }}
+                    className="w-full h-48 rounded-md mb-2"
+                    resizeMode="cover"
+                  />
+                )}
+
+                <View className="flex-row items-end mt-2">
+                  <Text className="text-xs text-[#8899A6] ml-auto">{formatDate(item.creationDate)}</Text>
+                </View>
+              </View>
             </TouchableOpacity>
-
-            {item.picture && (
-              <Image
-                source={{ uri: item.picture }}
-                className="w-full h-48 rounded-md mb-2"
-                resizeMode="cover"
-              />
-            )}
-
-
-
-            <View className="flex-row items-end mt-2">
-              <Text className="text-xs text-[#8899A6] ml-auto">{formatDate(item.creationDate)}</Text>
-            </View>
-
-          </View>
-        )}
+          );
+        }}
         contentContainerStyle={{ paddingBottom: 80 }}
       />
 
