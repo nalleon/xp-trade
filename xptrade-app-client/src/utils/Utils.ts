ce70aad0fc58cbba3420e9847c8518505659913e
mackstm <thelewyntm@gmail.com>
2025-05-15 16:13:08 +0100

    fix(utils): server IP change

--- xptrade-app-client/src/utils/Utils.ts
+++ xptrade-app-client/src/utils/Utils.ts
@@ -1,7 +1,7 @@
-export const URL_API = "http://172.17.0.1:8080/api";
+export const URL_API = "http://192.168.58.29:8080/api";
 export const PULLING_INTERVAL = 500;
 // Nabil: 172.17.0.1
-// Maxi: 192.168.1.117
+// Maxi: 192.168.1.117, 192.168.58.29
 
 export const SUCCESS = "success";
 export const RAWG_API_SEARCH = "https://api.rawg.io/api/games?key=1a094ebbcbef4440a46abee88f222abb&search_precise=true&search=";
