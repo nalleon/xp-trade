eca3bec7044f77b0b5851ffa8ec3be281caf46c0
nalleon <nabil14716@gmail.com>
2025-05-20 12:47:44 +0100

    fix(app-client): #38 Fixed navigation and partially logout

--- xptrade-app-client/src/navigations/RootNavigation.tsx
+++ xptrade-app-client/src/navigations/RootNavigation.tsx
@@ -1,32 +1,15 @@
-import React, { useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { ActivityIndicator, View } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import AuthStackNav from './stack/AuthStackNav';
 import DrawerNav from './drawer/DrawerNav';
+import { AppContext } from '../context/AppContext';
 
 const RootNavigation = () => {
-  const [loading, setLoading] = useState(true);
-  const [token, setToken] = useState<string | null>(null);
+  const context = useContext(AppContext);
 
-  useEffect(() => {
-    const checkToken = async () => {
-      const storedToken = await AsyncStorage.getItem('token');
-      setToken(storedToken);
-      setLoading(false);
-    };
-
-    checkToken();
-  }, []);
-
-  if (loading) {
-    return (
-      <View className="flex-1 justify-center items-center bg-[#0F1218]">
-        <ActivityIndicator size="large" color="#ffffff" />
-      </View>
-    );
-  }
-
-  return token ? <DrawerNav key="drawer" /> : <AuthStackNav key="auth" />;
+  return context.token ? <DrawerNav key="drawer" /> : <AuthStackNav key="auth" />;
 };
+
 
 export default RootNavigation;
--- xptrade-app-client/src/navigations/drawer/CustomDrawerContent.tsx
+++ xptrade-app-client/src/navigations/drawer/CustomDrawerContent.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import { InteractionManager, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React, { useContext } from 'react'
 import Icon from 'react-native-vector-icons/Ionicons';
 import { DrawerContentComponentProps, DrawerContentScrollView, DrawerItemList } from '@react-navigation/drawer'
@@ -10,17 +10,20 @@ type Props = {}
 
 const CustomDrawerContent = (props: DrawerContentComponentProps) => {
   const context = useContext(AppContext);
+  const navigation = useNavigation();
+  async function handleLogout() {
+    await AsyncStorage.clear();
+      context.setToken(null);
+  }
+
   return (
     <DrawerContentScrollView {...props}>
       <DrawerItemList {...props} />
-      <TouchableOpacity
-        onPress={() => props.navigation.navigate('AuthStackNav')}
-        className="flex-row items-center space-x-3 p-3 rounded-bl-xl rounded-tr-xl bg-[#442222]"
-      >
+      <TouchableOpacity onPress={handleLogout}
+        className="flex-row items-center space-x-3 p-3 rounded-bl-xl rounded-tr-xl bg-[#442222]">
         <Icon name="log-out-outline" size={20} color="#F6F7F7" />
-        <Text className="text-[#F6F7F7] text-base">Cerrar sesión</Text>
+        <Text className="text-[#F6F7F7] text-base">Cerrar sesión</Text>
       </TouchableOpacity>
-
     </DrawerContentScrollView>
   )
 }
